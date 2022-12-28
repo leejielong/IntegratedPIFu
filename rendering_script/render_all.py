be634178fce2a139e42f653764080ab289d90cc9
leejielong <leejielong@gmail.com>
2022-12-28 14:30:36 +0800

    feat: Create render_all script for mass image rendering

--- rendering_script/render_all.py
+++ rendering_script/render_all.py
@@ -2,7 +2,7 @@ from tqdm import tqdm
 import numpy as np
 import os
 
-training_subject_list = np.loadtxt("train_set_list.txt", dtype=str)
+training_subject_list = np.loadtxt("test_set_list.txt", dtype=str)
 training_subject_list = training_subject_list.tolist()
 root_dir = "rendering_script/buffer_fixed_full_mesh"
 angles = [0,45,90,135,180,225,270,315]
